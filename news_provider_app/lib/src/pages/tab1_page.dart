8b9606450aac0b70e0380453615c8263891b047e
Jmendozase6 <Jmendozase6@ucvvirtual.edu.pe>
2022-12-21 21:48:53 -0500

    [Add] Loading al seleccionar una categoría

--- news_provider_app/lib/src/pages/tab1_page.dart
+++ news_provider_app/lib/src/pages/tab1_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:news_provider_app/src/services/news_service.dart';
+import 'package:news_provider_app/src/theme/app_theme.dart';
 import 'package:news_provider_app/src/widgets/list_news.dart';
 import 'package:provider/provider.dart';
 
@@ -18,10 +19,23 @@ class _Tab1PageState extends State<Tab1Page>
     super.build(context);
     final headlines = Provider.of<NewsService>(context).headlines;
 
-    return Scaffold(
-      body: (headlines.isEmpty)
-          ? const Center(child: CupertinoActivityIndicator())
-          : ListNews(news: headlines),
+    return SafeArea(
+      child: Scaffold(
+        body: (headlines.isEmpty)
+            ? const Center(child: CupertinoActivityIndicator())
+            : Column(
+                children: [
+                  const SizedBox(height: 20),
+                  const Text('Noticias - México',
+                      style:
+                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+                  const SizedBox(height: 10),
+                  Container(height: 10, color: primaryColor),
+                  const SizedBox(height: 10),
+                  Expanded(child: ListNews(news: headlines)),
+                ],
+              ),
+      ),
     );
   }
 
